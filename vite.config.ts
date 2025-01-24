bfd30c11ddeb701d2f1138b093c8f2448c32406c
Aman2125 <amankumar943087@gmail.com>
2025-01-24 17:02:24 +0530

    Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/To_Do_Dashboard/', // Replace with your repo name
 });
